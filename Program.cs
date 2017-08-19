265e336d8e200e66a00d31a1503ac8abe247ba4a
Mark Dewey <mark.t.dewey@gmail.com>
2017-08-18 20:10:59 -0400

    base commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var service = new Sample();
+            var sender = new EmailSender();
+            service.SendEmail(sender);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Sample.cs
@@ -0,0 +1,9 @@
+// namespace DependencyInjection {
+
+//     public class Sample 
+//     {
+//         public void SendEmail(IEmailSender emailSender){
+//             emailSender.SendEmail("Hello world, check our sales!!!!"); 
+//         }
+//     }
+// }
